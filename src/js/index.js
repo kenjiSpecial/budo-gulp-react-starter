9994d45b83aec3108e8cf64c805d29af8009dd79
kenji-special <k.saito.1985@gmail.com>
2015-05-07 16:09:06 +0900

    add suss files and make simple example for starter kit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
+var React = require('react');
+window.React = React;
+
+class HelloMessage extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
+}
 
 require('domready')(() => {
   //show canvas demo
-    alert('domready');
+    React.render(<HelloMessage name="Sebastian" />, document.body);
 });
